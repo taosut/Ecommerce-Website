e2be259f2de1bd09ec2a9e71704b5cf73199f4c9
Dhiru Sarania <dhirusarania@gmail.com>
2020-05-14 17:14:46 +0530

    push

--- .nuxt/google-analytics.js
+++ .nuxt/google-analytics.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
 export default async (ctx, inject) => {
-  const options = {"dev":true,"debug":{"sendHitTask":false},"id":"UA-166644677-1"}
+  const options = {"dev":true,"debug":{"sendHitTask":true},"id":"UA-166644677-1"}
 
   if (typeof options.asyncID === 'function') {
     options.id = await options.asyncID(ctx)
